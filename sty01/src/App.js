5e804feca0f11375b89ae6683e4a8c365122bc99
YuYoHan <zxzz8014@naver.com>
2022-10-14 17:48:34 +0900

    [Study] React

--- sty01/src/App.js
+++ sty01/src/App.js
@@ -1,5 +1,22 @@
+import { userState } from "react";
+
 function App() {
-    return <div className="App"></div>;
+    const [number, setNumber] = userState(1);
+    const [number1, setNumber1] = userState(1);
+
+    const double = () => {
+        const doubledNumber = number * 2;
+        setNumber(doubledNumber);
+        setNumber1(number1 * 2);
+        console.log(doubledNumber);
+    };
+
+    return (
+        <>
+            <box>{number}</box>
+            <button onClick={double}>submit</button>
+        </>
+    );
 }
 
 export default App;
