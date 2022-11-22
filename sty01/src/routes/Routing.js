c4c7ba8a82ccb14ba7d54f4e1da27f75daa95dbf
YuYoHan <zxzz8014@naver.com>
2022-11-22 18:53:04 +0900

    [Study] Signup

--- sty01/src/routes/Routing.js
+++ sty01/src/routes/Routing.js
@@ -1,14 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import UseState2 from "../../../sty01/src/pages/hooks/useState2";
-import HomePage from "../pages/Home";
-import State from "../pages/hooks/useState";
+import SignUp from "../pages/SignUp";
 
 function Routing() {
     return (
         <BrowserRouter>
             <Routes>
-                {/* <Route path={"/1"} element={<State />} /> */}
-                <Route path={"/2"} element={<UseState2 />} />
+                <Route path={"/"} element={<SignUp />} />
             </Routes>
         </BrowserRouter>
     );
