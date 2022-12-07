197a4c7f72369382ad2caabb44b0a48ba153d4cf
YuYoHan <zxzz8014@naver.com>
2022-12-08 01:04:36 +0900

    [Study] useCallback

--- hooks/src/components/3_useCallback.js
+++ hooks/src/components/3_useCallback.js
@@ -5,11 +5,14 @@ function callBack() {
     const [number, setNumber] = useState(0);
     const [toggle, setToggle] = useState(true);
 
+    // number에 의존성을 주어서 바뀌면 콘솔이 출력
     const someFuntion = useCallback(() => {
         console.log(`someFunc: number: ${number}`);
         return;
     }, [number]);
 
+    // someFuntion에 의존성을 주고 이게 바뀌면 콘솔이 나온다.
+    // someFuntion이 바뀌지 않으면 콘솔은 처음에만 나오고 나오지 않는다.
     useEffect(() => {
         console.log(`someFunction이 변경되었습니다.`);
     }, [someFuntion]);
