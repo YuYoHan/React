73ffdcf003929e4b31b89be55c012a76bc216d11
YuYoHan <zxzz8014@naver.com>
2024-05-29 21:03:57 +0900

    [Study] Book 003

--- book/src/App.js
+++ book/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import ImportComponent from './folder/R003_ImportComponent'
 
 function App() {
     return (
         <div>
             <h1>Start React 200!</h1>
             <p>CSS 적용하기</p>
+            <ImportComponent/>
         </div>
     );
 }
