6faabeaae9e980115e547a1cf18ee817aa8836b4
YuYoHan <zxzz8014@naver.com>
2024-07-31 12:41:28 +0900

    [Study] 인프런 : 한입크기로 먹는 리액트

--- oneReact/src/App.jsx
+++ oneReact/src/App.jsx
@@ -1,33 +1,14 @@
 import "./App.css";
 import { useState } from "react";
+// 사용하려는 파일에서
+import Bulb from "./components/Bulb";
+import Counter from "./components/Counter";
 
 function App() {
-    const [count, setCount] = useState(0);
-    console.log(count);
-    const [light, setLight] = useState("OFF");
-
     return (
         <>
-            <div>
-                <h1>{light}</h1>
-                <button
-                    onClick={() => {
-                        setLight(light === "ON" ? "OFF" : "ON");
-                    }}
-                >
-                    {light === "ON" ? "끄기" : "켜기"}
-                </button>
-            </div>
-            <div>
-                <h1>{count}</h1>
-                <button
-                    onClick={() => {
-                        setCount(count + 1);
-                    }}
-                >
-                    +
-                </button>
-            </div>
+            <Bulb />
+            <Counter />
         </>
     );
 }
