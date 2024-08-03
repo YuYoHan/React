11bf7bab90154bce75924a9515bd276ccb53de0f
YuYoHan <zxzz8014@naver.com>
2024-08-03 09:41:14 +0900

    [Study] mockData 추가

--- toDo/src/App.jsx
+++ toDo/src/App.jsx
@@ -4,8 +4,29 @@ import List from "./components/List";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 
+// 중복으로 생성되지 않게 외부에서 생성
+const mockData = [
+    {
+        id: 0,
+        isDone: false,
+        content: "React 공부하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 1,
+        isDone: false,
+        content: "설거지하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        content: "OAuth2.0 공부하기",
+        date: new Date().getTime(),
+    },
+];
 function App() {
-    const [count, setCount] = useState(0);
+    const [todos, setTodos] = useState(mockData);
 
     return (
         <div className="App">
