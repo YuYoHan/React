153a0bef7b97f4bba2ae02b7d2119cce59fe28e4
YuYoHan <zxzz8014@naver.com>
2024-08-05 23:28:01 +0900

    [Study] 리액트

--- zero/src/pages/Product.js
+++ zero/src/pages/Product.js
@@ -14,13 +14,15 @@ function Product({ id, title, image, price, rating }) {
                 <div className="product_rating">
                     {
                         /*  배열을 rating만큼 돌리겠다는 뜻이고
-                            fill은 채우겠다는 뜻이고 map은 채울 것을 나타내는 것이다. */
+                            fill은 채우겠다는 뜻이고 map은 채울 것을 나타내는 것이다. 
+                        */
                         Array(rating)
                             .fill()
                             .map(() => (
                                 <p>★</p>
                             ))
-                    };
+                    }
+                    ;
                 </div>
             </div>
             <img src={image} alt="img" id="img1" />
