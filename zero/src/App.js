40fb26b0d6ed064908133adb503cbf54d55c4d24
YuYoHan <zxzz8014@naver.com>
2024-08-06 20:55:18 +0900

    [Study] Reat

--- zero/src/App.js
+++ zero/src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Header from "./pages/Header.js";
 import Home from "./pages/Home.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
     return (
-        <div>
-            <Header />
-            <Home />
-        </div>
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/">
+                        <Header />
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
--- zero/src/pages/Home.js
+++ zero/src/pages/Home.js
@@ -9,7 +9,7 @@ function Home() {
                 <img className="home_image" src="/image.png" alt="home_image" />
                 <div className="home_row">
                     <Product
-                        id="2323"
+                        id="1"
                         title="제품1"
                         price={3000}
                         image="https://www.itworld.co.kr/files/itworld/ITW_202311_01/space-back-macbook-pro-open.jpg"
@@ -17,11 +17,29 @@ function Home() {
                     />
                 </div>
                 <div className="home_row">
-                    <Product />
-                    <Product />
+                    <Product
+                        id="2"
+                        title="제품2"
+                        price={3000}
+                        image="https://dimg.donga.com/wps/NEWS/IMAGE/2020/09/14/102930719.2.jpg"
+                        rating={5}
+                    />
+                    <Product
+                        id="3"
+                        title="제품3"
+                        price={3000}
+                        image="https://p4.wallpaperbetter.com/wallpaper/202/872/950/car-lamborghini-huracan-lp-610-4-edit-black-wallpaper-preview.jpg"
+                        rating={5}
+                    />
                 </div>
                 <div className="home_row">
-                    <Product />
+                    <Product
+                        id="4"
+                        title="제품4"
+                        price={3000}
+                        image="https://shs-dome3.com/data/editor/2304/d8051cbb3740b4fe8d23cd2f44145f06_1681120090_3317.jpg"
+                        rating={5}
+                    />
                 </div>
             </div>
         </div>
