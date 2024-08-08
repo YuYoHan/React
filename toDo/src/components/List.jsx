6bcf08675a6d834d81dda7e42599296d5dc00834
YuYoHan <zxzz8014@naver.com>
2024-08-08 09:39:01 +0900

    [Study] 투두리스트 삭제하기

--- toDo/src/components/List.jsx
+++ toDo/src/components/List.jsx
@@ -2,43 +2,43 @@ import "./List.css";
 import TodoItem from "./TodoItem";
 import { useState } from "react";
 
-const List = ({ todos }) => {
-    const [search, setSearch] = useState("");
+const List = ({ todos, onUpdate }) => {
+  const [search, setSearch] = useState("");
 
-    const onChangeSearch = (e) => {
-        setSearch(e.target.value);
-    };
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
-    const getFilteredData = () => {
-        if (search === "") {
-            return todos;
-        }
-        // 배열의 모든 item을 순회하면서 연산의 결과 값이 true만 반환한다.
-        // content에 검색한 search를 포함하는 item만 return
-        return todos.filter((todo) =>
-            todo.content.toLowerCase().includes(search.toLowerCase())
-        );
-    };
-
-    const filteredTodos = getFilteredData();
-
-    return (
-        <div className="List">
-            <h4>Todo List</h4>
-            <input
-                placeholder="검색어를 입력하세요"
-                value={search}
-                onChange={onChangeSearch}
-            />
-            <div className="todos_wrapper">
-                {filteredTodos.map((todo) => {
-                    // TodoItem에 todo에 담긴 모든 데이터를 보내준다.
-                    // 리스트로 보낼 때는 키 값을 설정해야 한다.
-                    return <TodoItem key={todo.id} {...todo} />;
-                })}
-            </div>
-        </div>
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    // 배열의 모든 item을 순회하면서 연산의 결과 값이 true만 반환한다.
+    // content에 검색한 search를 포함하는 item만 return
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
     );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  return (
+    <div className="List">
+      <h4>Todo List</h4>
+      <input
+        placeholder="검색어를 입력하세요"
+        value={search}
+        onChange={onChangeSearch}
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          // TodoItem에 todo에 담긴 모든 데이터를 보내준다.
+          // 리스트로 보낼 때는 키 값을 설정해야 한다.
+          return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default List;
--- toDo/src/components/TodoItem.jsx
+++ toDo/src/components/TodoItem.jsx
@@ -1,14 +1,23 @@
 import "./TodoItem.css";
 
-const TodoItem = ({ id, isDone, content, date }) => {
-    return (
-        <div className="TodoItem">
-            <input type="checkbox" checked={isDone} readOnly />
-            <div className="content">{content}</div>
-            <div className="date">{new Date(date).toLocaleDateString()}</div>
-            <button>삭제</button>
-        </div>
-    );
+const TodoItem = ({ id, isDone, content, date, onUpdate }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
+  return (
+    <div className="TodoItem">
+      <input
+        onChange={onChangeCheckbox}
+        type="checkbox"
+        checked={isDone}
+        readOnly
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button>삭제</button>
+    </div>
+  );
 };
 
 export default TodoItem;
