525146666af988bc9830d1084e8bb32dd1808637
YuYoHan <zxzz8014@naver.com>
2024-08-19 09:51:12 +0900

    [Study] useMemo로 최적화

--- toDo/src/components/Header.jsx
+++ toDo/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import { memo } from "react";
 
 const Header = () => {
   return (
@@ -8,5 +9,9 @@ const Header = () => {
     </div>
   );
 };
-
-export default Header;
+// 최적화하기 위한 동작
+// 이렇게 처리하는 이유는 헤더가 불필요하게 리렌더링하기 때문에
+// 불필요한 리렌더링을 막기위해서 최적화
+const memoizedHeader = memo(Header);
+// 일반적으로 내보네는 Header가 아니라 최적화 시킨 것을 내보냄
+export default memoizedHeader;
--- toDo/src/components/TodoItem.jsx
+++ toDo/src/components/TodoItem.jsx
@@ -1,27 +1,39 @@
 import "./TodoItem.css";
+import { memo } from "react";
 
 const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
-    const onChangeCheckbox = () => {
-        onUpdate(id);
-    };
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
 
-    const onClickDelete = () => {
-        onDelete(id);
-    };
+  const onClickDelete = () => {
+    onDelete(id);
+  };
 
-    return (
-        <div className="TodoItem">
-            <input
-                onChange={onChangeCheckbox}
-                type="checkbox"
-                checked={isDone}
-                readOnly
-            />
-            <div className="content">{content}</div>
-            <div className="date">{new Date(date).toLocaleDateString()}</div>
-            <button onClick={onClickDelete}>삭제</button>
-        </div>
-    );
+  return (
+    <div className="TodoItem">
+      <input
+        onChange={onChangeCheckbox}
+        type="checkbox"
+        checked={isDone}
+        readOnly
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onClickDelete}>삭제</button>
+    </div>
+  );
 };
 
-export default TodoItem;
+// 교차 컴포넌트(HOC)
+export default memo(TodoItem, (prevProps, nextProps) => {
+  // 반환값에 따라 Props가 바뀌었는지 안바뀌었는지 판단
+  // true -> Props 바뀌지 않음 -> 리렌더링 x
+  // false -> Props 바뀜 -> 리렌더링 o
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+
+  return true;
+});
