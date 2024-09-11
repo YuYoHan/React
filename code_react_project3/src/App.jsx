f7b54ad680c39d5e205306fecfed5326047329a5
YuYoHan <zxzz8014@naver.com>
2024-09-11 16:56:15 +0900

    [Study] Route & Link & useNavigate

--- code_react_project3/src/App.jsx
+++ code_react_project3/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
@@ -9,13 +9,27 @@ import NotFound from "./pages/NotFound";
 // 2. "/new" : 새로운 일기를 작성하는 new 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <div>
+        <Link to={"/"}>Home</Link> <br />
+        <Link to={"/new"}>New</Link> <br />
+        <Link to={"/diary"}>Diary</Link> <br />
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
