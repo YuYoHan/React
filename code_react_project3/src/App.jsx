161072089603d0e39faa16a9bdc58fc69e969088
YuYoHan <zxzz8014@naver.com>
2024-09-12 16:25:44 +0900

    [Study] Header & Button 컴포넌트

--- code_react_project3/src/App.jsx
+++ code_react_project3/src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import NotFound from "./pages/NotFound";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 new 페이지
@@ -17,12 +19,33 @@ function App() {
 
   return (
     <>
-      <div>
-        <Link to={"/"}>Home</Link> <br />
-        <Link to={"/new"}>New</Link> <br />
-        <Link to={"/diary"}>Diary</Link> <br />
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+
+      <Button
+        text={"123"}
+        type={"DEFAULT"}
+        onClick={() => {
+          console.log("Button clicked");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("Button clicked");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("Button clicked");
+        }}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
