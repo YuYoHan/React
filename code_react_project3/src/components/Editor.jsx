7c2c0aa6242b82b35e44783d202fe872e40840b2
YuYoHan <zxzz8014@naver.com>
2024-09-14 10:37:56 +0900

    [Study] content 작성

--- code_react_project3/src/components/Editor.jsx
+++ code_react_project3/src/components/Editor.jsx
@@ -41,7 +41,7 @@ const getStringDate = (targetDate) => {
     return `${year}-${month}-${date}`;
 };
 
-const Editor = () => {
+const Editor = ({ onSubmit }) => {
     const [input, setInput] = useState({
         createdDate: new Date(),
         emotionId: 3,
@@ -59,7 +59,10 @@ const Editor = () => {
         }));
     };
 
-    const emotionId = 2;
+    const onSubmitButton = () => {
+        onSubmit(input);
+    };
+
     return (
         <div className="Editor">
             <section className="date_section">
@@ -76,20 +79,37 @@ const Editor = () => {
                 <div className="emotion_list_wrapper">
                     {emotionList.map((item) => (
                         <EmotionItem
+                            onClick={() =>
+                                onChangeInput({
+                                    target: {
+                                        name: "emotionId",
+                                        value: item.emotionId,
+                                    },
+                                })
+                            }
                             key={item.emotionId}
                             {...item}
-                            isSelected={item.emotionId === emotionId}
+                            isSelected={item.emotionId === input.emotionId}
                         />
                     ))}
                 </div>
             </section>
             <section className="content_section">
                 <h4>오늘의 일기</h4>
-                <textarea placeholder="오늘은 어땠나요?" />
+                <textarea
+                    name="content"
+                    value={input.content}
+                    onChange={onChangeInput}
+                    placeholder="오늘은 어땠나요?"
+                />
             </section>
             <section className="button_section">
                 <Button text={"취소하기"} />
-                <Button text={"작성완료"} type={"POSITIVE"} />
+                <Button
+                    onClick={onSubmitButton}
+                    text={"작성완료"}
+                    type={"POSITIVE"}
+                />
             </section>
         </div>
     );
--- code_react_project3/src/components/EmotionItem.jsx
+++ code_react_project3/src/components/EmotionItem.jsx
@@ -1,17 +1,22 @@
 import "./EmotionItem.css";
 import { getEmotionImage } from "../util/get-emotion-image";
 
-const EmotionItem = ({ emotionId, emotionName, isSelected }) => {
-  return (
-    <div
-      className={`EmotionItem ${
-        isSelected ? `EmotionItem_on_${emotionId}` : ""
-      }`}
-    >
-      <img className="emotion_img" src={getEmotionImage(emotionId)} />
-      <div className="emotion_name">{emotionName}</div>
-    </div>
-  );
+const EmotionItem = ({ emotionId, emotionName, isSelected, onClick }) => {
+    return (
+        <div
+            className={`EmotionItem ${
+                isSelected ? `EmotionItem_on_${emotionId}` : ""
+            }`}
+            onClick={onClick} // 클릭 이벤트 추가
+        >
+            <img
+                className="emotion_img"
+                src={getEmotionImage(emotionId)}
+                alt={emotionName}
+            />
+            <div className="emotion_name">{emotionName}</div>
+        </div>
+    );
 };
 
 export default EmotionItem;
